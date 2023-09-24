abb268c0a8866460d883d8abd5206b0240ec4f90
mansidethe <mansidethe18@gmail.com>
2023-09-24 17:24:12 +0530

    initial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+import Contact from "./contact/Contact"
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <div>
+    <Contact/>
+  </div>
+);
+
